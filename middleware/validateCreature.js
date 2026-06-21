cba644be55bc202ff95573401b47c87752e1b7c6
userlaaame <daddystarbuqs@disroot.org>
2026-06-21 03:31:19 -0400

    add custom middleware #2

---
+++ middleware/validateCreature.js
@@ -0,0 +1,10 @@
+//custom middleware #2 the gatekeeper
+module.exports = function validateCreature(req, res, next) {
+    const { name, type } = req.body;
+    if (!name || !type) {
+        const err = new Error('Both "name" and "type" are required'); 
+        err.status = 400;
+        return next(err);//The actual check: if either field is missing, reject.
+    }
+    next();
+};
